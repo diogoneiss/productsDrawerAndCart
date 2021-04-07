3a24c86629a180f0640c0e3fd9b43c2b61543390
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2021-04-07 02:09:30 -0300

    Criação de rotas e lógica do redux

---
+++ src/Store.tsx
@@ -0,0 +1,34 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import React from "react";
+
+const persistenceConfigs = {
+   key: "estadoApp", // chave no localStorage
+   storage
+};
+const persistedReducer = persistReducer(persistenceConfigs, rootReducer);
+//visualizar store com o chrome
+const store = createStore(
+   persistedReducer,
+   composeWithDevTools(
+      applyMiddleware(thunk),
+      // other store enhancers if any
+   ),
+);
+export const persistor = persistStore(store);
+
+const ReduxWrapper: React.FC = (props) => {
+   return (
+      <Provider store={store}>
+         <PersistGate persistor={persistor}>{props.children}</PersistGate>
+      </Provider>
+   );
+};
+
+export default ReduxWrapper;
---
+++ src/reducers/cartReducer.tsx
@@ -0,0 +1,115 @@
+import {AnyAction} from 'redux'
+
+type Product = {
+    id: String,
+    quantity: number,
+    name: string,
+    price: number,
+    img: string
+}
+
+type InitialStateType = {
+    products: Product[] | [],
+    totalPrice: number
+}
+
+//estado inicial, com tipos inferidos
+const initialState: InitialStateType = {
+    products: [],
+    totalPrice: 0,
+}
+
+export const ACTION_TYPES = {
+    ADD_TO_CART: "ADD_TO_CART",
+    REMOVE_FROM_CART: "REMOVE_FROM_CART",
+    CHANGE_QUANTITY: "CHANGE_QUANTITY"
+}
+
+const cartReducer = (state = initialState, action: AnyAction) => {
+    switch(action.type){
+        case ACTION_TYPES.ADD_TO_CART: {
+
+
+            const addedProduct: Product = action.payload;
+
+            //checking if product already on cart
+            let found = false;
+            let i;
+            let newProductArray = state.products;
+
+            //verificando se o id já existe dentro do array de produtos
+            for(i = 0; i < state.products.length; ++i){
+                if(state.products[i].id === addedProduct.id){
+                    found = true;
+                    break;
+                }
+            }
+
+            //se o produto já existir, aumentar a quantidade
+            if(found){
+                newProductArray[i].quantity += 1 ;
+            }
+            else{
+                newProductArray = [...newProductArray, addedProduct]
+            }
+
+
+            return {
+                totalPrice: addedProduct.price + state.totalPrice,
+                products: newProductArray
+            }
+        }
+        case ACTION_TYPES.CHANGE_QUANTITY: {
+
+            const changedProduct: Product = action.payload.product;
+            const changedQuantity: number = action.payload.change
+
+            //checking if product already on cart
+           
+            let i;
+            let newProductArray = state.products;
+
+            //calculando o índice do produto
+            for(i = 0; i < state.products.length; ++i){
+                if(state.products[i].id === changedProduct.id){
+                    break;
+                }
+            }
+
+            //se o produto tiver quantidade igual a 1, remover definitivamente
+            // do carrinho
+            if(newProductArray[i].quantity === 1){
+                //array sem o produto
+               newProductArray = newProductArray.filter(product => product.id !== changedProduct.id)
+            }
+            //variar na quantidade especificada
+            else{
+                newProductArray[i].quantity += changedQuantity;
+            }
+
+
+            return {
+                totalPrice: state.totalPrice + changedQuantity * changedProduct.price,
+                products: newProductArray
+            }
+
+        }
+        case ACTION_TYPES.REMOVE_FROM_CART: {
+            const removedProduct: Product = action.payload;
+          
+
+            //checking if product already on cart
+           
+           
+            const newProductArray = state.products.filter(product => product.id !== removedProduct.id)
+
+            return {
+                totalPrice: state.totalPrice - removedProduct.price,
+                products: newProductArray
+            }
+        }
+        default: return initialState;
+    }
+}
+
+export default cartReducer;
